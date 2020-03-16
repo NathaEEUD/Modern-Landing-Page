a88708a4750e3fb079b89d4f4bbf2828fb0530f8
Nathaly Riaño Bejarano <natharb09@gmail.com>
2020-03-16 16:47:13 -0500

    feat: adding media queries for responsive design

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const $button = document.getElementById("header__menu");
 const $links = document.getElementById("header__links");
 
-$button.addEventListener('click', () => {
+$button.addEventListener('click', (e) => {
+  e.preventDefault();
   $links.classList.toggle('header__menu--close');
   $links.classList.toggle('header__menu--open');
 })
